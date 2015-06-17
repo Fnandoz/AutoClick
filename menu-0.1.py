d076e5d5fbfdf5c447689ad6791c692c7bc9cc09
Fnandoz <lfgsnando@hotmail.com>
2015-06-16 22:45:17 -0300

    Create menu-0.1.py
    Menu

---
+++ menu-0.1.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+while(True):
+    botao = pyautogui.confirm(text='Auto Click', title='Auto Click', buttons=['Iniciar','Sair'])
+
+    if(botao == 'Iniciar'):
+        import AUTOCLICK01.py
+    else:
+        break
