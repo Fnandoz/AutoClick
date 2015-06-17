d42774002147ee110b7263d00a01300673c104f5
Fnandoz <lfgsnando@hotmail.com>
2015-06-16 22:44:42 -0300

    Create PyAutoClick.py

---
+++ PyAutoClick.py
@@ -0,0 +1,24 @@
+import pyautogui
+import sys
+import time
+
+pyautogui.alert(text = "Gravar posição 1 do cursor", title = 'Auto click')
+time.sleep(5)
+pos1 = pyautogui.position()
+pyautogui.alert(text = "Posição 1 gravada", title = 'Auto click')
+
+pyautogui.alert(text = "Gravar posição 2 do cursor", title = 'Auto click')
+time.sleep(5)
+pos2 = pyautogui.position()
+pyautogui.alert(text = "Posição 2 gravada", title = 'Auto click')
+
+pyautogui.alert(text = "Iniciando...",title = "Auto click")
+try:
+    while(True):
+        pyautogui.click(pos1,button='left')
+        #print("Clicando")
+        pyautogui.click(pos2, button = 'left')
+        #print("Clicando")
+        
+except KeyboardInterrupt:
+    pyautogui.alert(text = "Saindo...", title = 'Auto click')
